d66d4386ce78e997231cc17b18d093549256ba57
Vũ Văn Anh <vuvananh010203@gmail.com>
2026-02-28 19:00:33 +0700

    feat: Redesign the application logo, replace the favicon with a new SVG icon, and rename the logo component.

--- src/@core/svg/Logo.tsx
+++ src/@core/svg/Logo.tsx
@@ -3,33 +3,23 @@ import type { SVGAttributes } from 'react';
 
 const Logo = (props: SVGAttributes<SVGElement>) => {
   return (
-    <svg width='1.4583em' height='1em' viewBox='0 0 35 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-      <path
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M0.00188479 0V7.47707C0.00188479 7.47707 -0.145285 9.83135 2.161 11.8242L14.9358 23.9961L21.5792 23.9107L20.5136 10.7809L17.9947 7.82497L10.0778 0H0.00188479Z'
-        fill='currentColor'
-      />
-      <path
-        opacity='0.06'
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M8.39807 17.9307L13.6581 3.53127L18.059 7.91564L8.39807 17.9307Z'
-        fill='#161616'
-      />
-      <path
-        opacity='0.06'
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M8.81183 17.3645L15.2093 5.06165L18.0926 7.94695L8.81183 17.3645Z'
-        fill='#161616'
-      />
-      <path
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M8.47955 17.8436L25.8069 0H34.9091V7.50963C34.9091 7.50963 34.7195 10.0128 33.4463 11.3517L21.5808 24H14.9387L8.47955 17.8436Z'
-        fill='currentColor'
-      />
+    <svg width="1.5em" height="1.5em" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <defs>
+        <linearGradient id="mainGrad" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" style={{ stopColor: '#00f2fe', stopOpacity: 1 }} />
+          <stop offset="100%" style={{ stopColor: '#4facfe', stopOpacity: 1 }} />
+        </linearGradient>
+        <filter id="glow" x="-20%" y="-20%" width="140%" height="140%">
+          <feGaussianBlur stdDeviation="3" result="blur" />
+          <feComposite in="SourceGraphic" in2="blur" operator="over" />
+        </filter>
+      </defs>
+      <path d="M50 0 L90 25 L90 75 L50 100 L10 75 L10 25 Z" fill="none" stroke="#2c3e50" strokeWidth="2" opacity="0.3" />
+      <path d="M50 10 L85 35 L50 60 L15 35 Z" fill="url(#mainGrad)" filter="url(#glow)">
+        <animate attributeName="opacity" values="0.7;1;0.7" dur="3s" repeatCount="indefinite" />
+      </path>
+      <path d="M50 90 L85 65 L50 40 L15 65 Z" fill="#1a2a6c" opacity="0.8" />
+      <circle cx="50" cy="50" r="8" fill="#fff" filter="url(#glow)" />
     </svg>
   );
 };
